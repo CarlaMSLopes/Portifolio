67403179210ae41ad69c09b76052a927fef087c0
CarlaMSLopes <carlalopes613@outlook.com>
2024-07-05 00:53:59 -0300

    :lipstick: feat: estilização da section projeto

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 let trilho = document.getElementById("trilho");
-let body = document.querySelector('body')
+let body = document.querySelector('body');
+let logo_h1 = document.getElementById('logo_h1');
+let iconImg =document.getElementById('iconImg');
 trilho.addEventListener('click', ()=>{
    trilho.classList.toggle('dark') 
    body.classList.toggle('dark')
+
+   /* Dark mode, header */
+   logo_h1.classList.toggle('dark')
+   iconImg.classList.toggle('dark')
+
 })
 
